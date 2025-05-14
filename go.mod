2ba456c7469383a2a54d8a667083173342f2ec64
Tamara <tomatenstrauch1234567890@gmail.com>
2025-05-14 17:49:54 +0200

    attempts to integrate parser-go (NONFUNC ATM)

--- go.mod
+++ go.mod
@@ -11,11 +11,19 @@ require (
 	github.com/huandu/xstrings v1.5.0
 	github.com/labstack/echo/v4 v4.12.0
 	github.com/rs/zerolog v1.34.0
+	github.com/santhosh-tekuri/jsonschema/v6 v6.0.1
 	github.com/spf13/cobra v1.9.1
 	golang.org/x/text v0.25.0
 )
 
 require (
-	github.com/santhosh-tekuri/jsonschema/v6 v6.0.1 // indirect
-	gopkg.in/yaml.v2 v2.3.0 // indirect
+	github.com/asyncapi/converter-go v0.3.0 // indirect
+	github.com/asyncapi/parser-go v0.5.0 // indirect
+	github.com/asyncapi/spec-json-schemas/v4 v4.3.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xeipuuv/gojsonpointer v0.0.0-20190809123943-df4f5c81cb3b // indirect
+	github.com/xeipuuv/gojsonreference v0.0.0-20180127040603-bd5ef7bd5415 // indirect
+	github.com/xeipuuv/gojsonschema v1.2.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -9,10 +9,62 @@ import (
 	"github.com/santhosh-tekuri/jsonschema/v6"
 )
 
+/*
+func main() {
+	// Read your AsyncAPI spec (YAML or JSON)
+	data, err := os.ReadFile("asyncapi.yaml")
+	if err != nil {
+		panic(fmt.Errorf("failed to read asyncapi.yaml: %w", err))
+	}
+
+	// Parse the document (strict mode: true)
+	doc, err := parser.Parse(data, parser.WithStrict(true))
+	if err != nil {
+		panic(fmt.Errorf("failed to parse AsyncAPI spec: %w", err))
+	}
+
+	// Access parsed data
+	info := doc.Info()
+	fmt.Println("Title:    ", info.Title())
+	fmt.Println("Version:  ", info.Version())
+	fmt.Println("Channels: ")
+
+	for name, ch := range doc.Channels().All() {
+		fmt.Printf("- %s: %s\n", name, ch.Description())
+	}
+}
+*/
+
 func main() {
 	//TODO Logging-Mechanismus später hinzufügen
 	schemaFile := "./examples/schemas/jsonSchema.json"
 	instanceFile := "./examples/asyncapiv3-min.json"
+
+	// Read file content
+	data, err := os.ReadFile(instanceFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = parser(data, os.Stdout)
+
+	/*// Parse the document (note: parser detects JSON or YAML automatically)
+	doc, err := parseschema.NewParser(data)
+	if err != nil {
+		log.Fatal(err)
+	}*/
+
+	// Access document information
+	info := doc.Info()
+	fmt.Println("AsyncAPI Title:", info.Title())
+	fmt.Println("   Version:       ", info.Version())
+	fmt.Println("   Description:   ", info.Description())
+
+	// Print available channels
+	fmt.Println("Channels:")
+	for name, channel := range doc.Channels().All() {
+		fmt.Printf("- %s: %s\n", name, channel.Description())
+	}
 
 	if fs.Check_YAML(schemaFile) {
 		fmt.Println(fs.ConvertYAMLIntoJSON(schemaFile))
