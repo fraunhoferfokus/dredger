023f2dbfe7e4bda58df137fbd2793f20b49f1a37
DieVeganeWurst <147806536+DieVeganeWurst@users.noreply.github.com>
2025-06-09 02:29:28 +0200

    fix parser status code detection

--- parser/parser_async_test.go
+++ parser/parser_async_test.go
@@ -3,12 +3,12 @@ package parser
 import "testing"
 
 func TestParseAsync(t *testing.T) {
-    // CWD = dredger/parser
-    doc, err := ParseAsyncAPISpecFile("../examples/hello_async.yaml")
-    if err != nil {
-        t.Fatalf("unexpected error: %v", err)
-    }
-    if doc.Info.Title != "HelloEvent" {
-        t.Fatalf("got title %q, want %q", doc.Info.Title, "HelloEvent")
-    }
+	// CWD = dredger/parser
+	doc, err := ParseAsyncAPISpecFile("../examples/hello_async.yaml")
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if doc.Info.Title != "HelloEvent" {
+		t.Fatalf("got title %q, want %q", doc.Info.Title, "HelloEvent")
+	}
 }
