980dcb630fc46ad6610daed9d93781bfa44ad6cb
liaem <Emi-ly@tu-berlin.de>
2025-06-14 17:56:29 +0200

    Format logs

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"embed"
+	"os"
 
 	"dredger/cli"
 
@@ -9,12 +10,20 @@ import (
 	// damit wir ihnen die eingebetteten Templates geben können:
 	genAsyncAPI "dredger/generator/asyncapi"
 	genOpenAPI "dredger/generator/openapi"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
 //go:embed templates
 var tmplFS embed.FS
 
 func main() {
+	// Set up zerolog time format
+	zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
+	// Set pretty logging on
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	zerolog.SetGlobalLevel(zerolog.DebugLevel)
 	// Hier übergeben wir die eingebetteten Dateien an die Generator-Packages:
 	genOpenAPI.TmplFS = tmplFS
 	genAsyncAPI.TmplFS = tmplFS
