9cb7b68e76f0753510eab54a49916e8150b39ff0
liaem <Emi-ly@tu-berlin.de>
2025-08-05 14:14:55 +0200

    Added function for async

--- generator/info.go
+++ generator/info.go
@@ -42,13 +42,21 @@ func generateInfoFilesAsync(spec *asyncapiv3.Specification, serverConf ServerCon
 	// info.go
 	fileName := "info.go"
 	filePath := filepath.Join(Config.Path, CorePkg, fileName)
+<<<<<<< HEAD
 	templateFile := "templates/common/core/info.go.tmpl"
+=======
+	templateFile := "templates/openapi/core/info.go.tmpl"
+>>>>>>> 8c640d3 (Added function for async)
 	createFileFromTemplate(filePath, templateFile, serverConf)
 
 	// infoSvc.go
 	fileName = "infoSvc.go"
 	filePath = filepath.Join(Config.Path, CorePkg, fileName)
+<<<<<<< HEAD
 	templateFile = "templates/common/core/infoSvc.go.tmpl"
+=======
+	templateFile = "templates/openapi/core/infoSvc.go.tmpl"
+>>>>>>> 8c640d3 (Added function for async)
 	if _, err := os.Stat(filePath); errors.Is(err, os.ErrNotExist) {
 		createFileFromTemplate(filePath, templateFile, serverConf)
 	}
