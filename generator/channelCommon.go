2f68b433d0424fa4642ba6d9e585ba1c0e824adc
liaem <Emi-ly@tu-berlin.de>
2025-08-14 12:08:11 +0200

    changed from making go-file from channelname to operationname

--- generator/channelCommon.go
+++ generator/channelCommon.go
@@ -50,7 +50,7 @@ func GenerateChannelFile(spec *asyncapiv3.Specification, conf GeneratorConfig) {
 	absPath, _ := filepath.Abs(tmplPath)
 	log.Info().Msgf("Loading template from: %s", absPath)
 	for _, c := range configs {
-		destPath := path.Join(fpath, camelcase(c.ChannelName)+".go")
+		destPath := path.Join(fpath, lcFirst(c.OperationName)+".go")
 		createFileFromTemplate(destPath, tmplPath, c)
 	}
 	log.Info().Msg("Finished generating all files for Publishers folder")
