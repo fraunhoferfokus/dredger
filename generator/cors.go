712a6946c917442fc09c589258693f1b58604a5e
Gero Buchholz <gero.buchholz@fokus.fraunhofer.de>
2026-04-09 17:20:41 +0200

    feat: move locales back to core

--- generator/cors.go
+++ generator/cors.go
@@ -1,6 +1,9 @@
 package generator
 
 import (
+	fs "dredger/fileUtils"
+	"errors"
+	"os"
 	"path/filepath"
 
 	"github.com/rs/zerolog/log"
@@ -14,4 +17,14 @@ func generateCorsConfig(conf GeneratorConfig) {
 	filePath := filepath.Join(Config.Path, MiddlewarePackage, fileName)
 	templateFile := "templates/openapi/middleware/cors.go.tmpl"
 	createFileFromTemplate(filePath, templateFile, conf)
+
+	// locales (localize for en+de)
+	targetCoreDir := filepath.Join(Config.Path, "core")
+	localesPath := filepath.Join(targetCoreDir, "locales")
+	fs.GenerateFolder(localesPath)
+	createFileFromTemplate(filepath.Join(targetCoreDir, "localize.go"), "templates/common/core/localize.go.tmpl", conf)
+	if _, err := os.Stat(filepath.Join(localesPath, "locale.de.toml")); errors.Is(err, os.ErrNotExist) {
+		createFileFromTemplate(filepath.Join(localesPath, "locale.de.toml"), "templates/common/core/locales/locale.de.toml", conf)
+		createFileFromTemplate(filepath.Join(localesPath, "locale.en.toml"), "templates/common/core/locales/locale.en.toml", conf)
+	}
 }
